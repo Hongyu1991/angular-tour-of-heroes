74a95c0d46a10834ebc15534fc1a9c700e067e3c
Hongyu <hy2504@columbia.edu>
2017-06-23 15:01:17 -0400

    add, delete with db

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
-import { RouterModule }   from '@angular/router';
-
+// import { RouterModule }   from '@angular/router';
+import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -10,29 +10,24 @@ import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent} from './dashboard.component';
 
+import { AppRoutingModule }     from './app-routing.module';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+
+
 @NgModule({
-  imports: [ 
+  imports: [
   	BrowserModule,
   	FormsModule,
-  	RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-  	  {
-  	    path: 'heroes',
-  	    component: HeroesComponent
-  	  }
-	 ])
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+  	AppRoutingModule
   ],
-  declarations: [ 
-    AppComponent, 
-    HeroDetailComponent, 
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
     HeroesComponent,
     DashboardComponent
   ],
